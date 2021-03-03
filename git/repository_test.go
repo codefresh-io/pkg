f49282ce30659a6c2dea54920739734eeb9c91be
Noam Gal <noam.gal@codefresh.io>
2021-03-03 17:53:41 +0200

    renamed and split tests

--- git/repository_test.go
+++ git/repository_test.go
@@ -23,43 +23,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewProvider(t *testing.T) {
-	tests := map[string]struct {
-		opts             *Options
-		expectedProvider Provider
-		expectedError    string
-	}{
-		"Github": {
-			&Options{
-				Type: "github",
-			},
-			&github{},
-			"",
-		},
-		"No Type": {
-			&Options{},
-			nil,
-			ErrProviderNotSupported.Error(),
-		},
-		"Bad Type": {
-			&Options{Type: "foo"},
-			nil,
-			ErrProviderNotSupported.Error(),
-		},
-	}
-
-	for name, test := range tests {
-		t.Run(name, func(t *testing.T) {
-			p, err := NewProvider(test.opts)
-			if test.expectedError != "" {
-				assert.EqualError(t, err, test.expectedError)
-				return
-			}
-			assert.IsType(t, test.expectedProvider, p)
-		})
-	}
-}
-
 func Test_Clone(t *testing.T) {
 	tests := map[string]struct {
 		opts             *CloneOptions
