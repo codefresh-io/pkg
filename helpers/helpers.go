ff904a061b92a3b12b87d32d93662e03c17380ca
Noam Gal <noam.gal@codefresh.io>
2021-03-10 14:26:21 +0200

    Added CopyDir

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -15,8 +15,11 @@ package helpers
 
 import (
 	"context"
+	"io/ioutil"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"strings"
 
 	"github.com/codefresh-io/pkg/log"
 )
@@ -42,4 +45,53 @@ func Die(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func CopyDir(source, destination string) error {
+	return filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		var relPath string = strings.Replace(path, source, "", 1)
+		if relPath == "" {
+			return nil
+		}
+
+		absDst := filepath.Join(destination, relPath)
+		if err = ensureDir(absDst); err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			err = os.Mkdir(absDst, info.Mode())
+			if err != nil {
+				if os.IsExist(err.(*os.PathError).Unwrap()) {
+					return nil
+				}
+			}
+
+			return err
+		} else {
+			data, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+
+			return ioutil.WriteFile(absDst, data, info.Mode())
+		}
+	})
+}
+
+func ensureDir(path string) error {
+	dstDir := filepath.Dir(path)
+	if _, err := os.Stat(dstDir); err != nil {
+		if !os.IsNotExist(err) {
+			return err
+		}
+
+		return os.MkdirAll(dstDir, 0755)
+	}
+
+	return nil
 }
